894db2d8537204f9a39ba3ed276ac645fdada9aa
rafael moreira <rafael@rafaelmoreira.org>
2018-03-19 00:39:07 -0300

    more emacs confs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,39 +1,3 @@
-;; load emacs 24's package system. Add MELPA repository.
-(require 'package)
-
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-
-(add-to-list 'package-archives '("marmalade" . "httpsl://marmalade-repo.org/packages"))
-
-(package-initialize)
-
-(require 'cl-lib) ;; dont know what this do
-
-(defvar my-packages
-  '(undo-tree railscasts-theme)
-  "A list of packages to ensure are installed at launch.")
-
-(defun my-packages-installed-p ()
-  (cl-loop for p in my-packages
-           when (not (package-installed-p p)) do (cl-return nil)
-           finally (cl-return t)))
-
-(unless (my-packages-installed-p)
-  ;; check for new packages (versions)
-  (package-refresh-contents)
-  ;; install missing packages
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,35 +5,138 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" default)))
- '(package-selected-packages (quote (railscasts-theme undo-tree))))
-
+    ("45482e7ddf47ab1f30fe05f75e5f2d2118635f5797687e88571842ff6f18b4d5" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "291588d57d863d0394a0d207647d9f24d1a8083bb0c9e8808280b46996f3eb83" default)))
+ '(package-selected-packages
+   (quote
+    (undo-tree color-theme-sanityinc-tomorrow zenburn-theme moe-theme dracula-theme counsel-projectile counsel ivy projectile molokai-theme color-theme-modern auto-complete ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 
-(global-set-key (kbd "C-x C-b") 'ibuffer) ;; default to ibuffer
-(global-hl-line-mode 1) ;; turn on highlight for the current line
-(global-linum-mode 1) ;; always display line numbers
-(column-number-mode 1) ;; always show the column number
-(show-paren-mode 1) ;; highlight matching parenthesis
-(save-place-mode 1) ;; remember cursor position
-(load-theme 'railscasts)
-;; set font type and size 
-(when (member "DejaVu Sans Mono" (font-family-list))
-  (set-face-attribute 'default nil :font "DejaVu Sans Mono-12"))
+(setq inhibit-startup-message t) ;; do not exibit the inicial message
+(tool-bar-mode -1)
 
-;; backup in one place. flat, no tree structure
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+    ))
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+(use-package color-theme-modern
+  :ensure t)
+
+(use-package molokai-theme
+  :ensure t)
+
+(use-package dracula-theme
+  :ensure t)
+
+(use-package moe-theme
+  :ensure t)
+
+(use-package zenburn-theme
+  :ensure t)
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config (projectile-mode))
+
+(use-package ivy
+	     :ensure t)
+
+(use-package counsel
+  :ensure t)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+
+(use-package counsel-projectile
+  :ensure t
+  :config (counsel-projectile-mode))
+
+(use-package undo-tree
+  :ensure t)
+
+
+
+;; ido configs
+(setq indo-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(defalias 'list-buffers 'ibuffer)
+
+(load-theme 'railscast)
+(set-frame-font "Dejavu sans mono 12" nil t)
+(global-hl-line-mode t) ;; hightlight the current line
+(electric-pair-mode 1) ;; auto add matching pair
+(show-paren-mode 1) ;; hightlight matching parens
+(global-linum-mode 1) ;; show line numbers
+(column-number-mode 1) ;; show column number
+(save-place-mode 1) ;; remeber cursos position
 (setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
 (savehist-mode 1) ;; save minibuffer history
-
-
-;; code preferences
-(electric-pair-mode 1) ;; auto insert the closing match (), {} and so on.
 (delete-selection-mode 1) ;; type over selected text
 
 ;; tabs / spaces
@@ -89,3 +156,4 @@
 ;; undo tree
 (global-undo-tree-mode 1)
 (setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
