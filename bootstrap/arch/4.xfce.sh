43096146f9b17c50a84507a1ec981ab93fda8091
rafael moreira <rafael@rafaelmoreira.org>
2018-03-25 18:05:09 -0300

    more configs for xfce

--- bootstrap/arch/4.xfce.sh
+++ bootstrap/arch/4.xfce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S xfce4 xfce4-goodies firefox chromium
+sudo pacman -S xfce4 xfce4-goodies firefox chromium ttf-dejavu kupfer
---
+++ bootstrap/arch/5.trizen.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+git clone https://aur.archlinux.org/trizen.git ~/trizen
+cd ~/trizen
+makepkg -si
